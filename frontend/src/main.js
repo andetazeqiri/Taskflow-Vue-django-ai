7735937e05247906c524e381f2285886774ea3c5
andetazeqiri <andeta_zeqiri.fshnstudent@unitir.edu.al>
2026-02-27 22:55:41 +0100

    configured primevue with aura theme and global components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,37 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 import App from './App.vue'
 import router from './router'
+
+
+import Toast from 'primevue/toast'
+import Dialog from 'primevue/dialog'
+import ConfirmDialog from 'primevue/confirmdialog'
+
+
+import ToastService from 'primevue/toastservice'
+import DialogService from 'primevue/dialogservice'
+import ConfirmationService from 'primevue/confirmationservice'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+app.use(ToastService)
+app.use(DialogService)
+app.use(ConfirmationService)
+
+app.component('Toast', Toast)
+app.component('Dialog', Dialog)
+app.component('ConfirmDialog', ConfirmDialog)
 
 app.mount('#app')
