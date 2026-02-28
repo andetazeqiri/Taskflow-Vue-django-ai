15ca57d34513a8d8b02d22c2d3fb1b1fb27f467b
andetazeqiri <andeta_zeqiri.fshnstudent@unitir.edu.al>
2026-02-28 22:05:45 +0100

    added filtering, searching and ordering support to task endpoint

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -8,9 +8,18 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
 	serializer_class = TaskSerializer
 	permission_classes = [IsAuthenticated]
+	
+	filterset_fields = ['status', 'due_date']
+	
+	search_fields = ['title', 'description']
+	
+	ordering_fields = ['created_at', 'updated_at', 'due_date', 'title', 'status']
+	ordering = ['-created_at']  
 
 	def get_queryset(self):
+		"""Return tasks filtered by authenticated user"""
 		return Task.objects.filter(user=self.request.user)
 
 	def perform_create(self, serializer):
+		"""Assign authenticated user to task on creation"""
 		serializer.save(user=self.request.user)
